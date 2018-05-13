6b038332f0ef5459d76970b5d7dbd3cff3c0b3e1
Ed Ilyin <ed.il@live.com>
2018-05-13 20:41:12 +0300

    butt

--- src/App/App.fs
+++ src/App/App.fs
@@ -7,7 +7,7 @@ type Model = { count: int }
 type Msg = Decrease | Increase | Reset
 let init () = { count = 1 }
 
-let button color text dispatch msg =
+let butt color text dispatch msg =
     button [
         Style [ BackgroundColor color; Color "white" ]
         On [ Click <| fun _ -> dispatch msg ]
@@ -15,10 +15,10 @@ let button color text dispatch msg =
 
 let view model dispatch =
     div [] [
-        button "blue" "-" dispatch Decrease
+        butt "blue" "-" dispatch Decrease
         div [] [ string model.count |> str ]
-        button "red" "+" dispatch Increase
-        button "green" "Reset" dispatch Reset
+        butt "red" "+" dispatch Increase
+        butt "green" "Reset" dispatch Reset
     ]
 
 let update cmd model =
